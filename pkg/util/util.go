e6198fa0a7f3ea2821d4a39e791e049b9b2a92ee
Song Song Li <ssli@redhat.com>
2020-07-23 19:23:13 +0800

    add deploy manifests

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,6 +1,13 @@
 package util
 
-import "os"
+import (
+	"os"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+)
 
 func GetEnv(key, defaultValue string) string {
 	if value, ok := os.LookupEnv(key); ok {
@@ -8,4 +15,29 @@ func GetEnv(key, defaultValue string) string {
 	}
 
 	return defaultValue
+}
+
+func GetKubeClient(kubeConfig string) kubernetes.Interface {
+	var (
+		err    error
+		config *rest.Config
+	)
+
+	if kubeConfig != "" {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeConfig)
+		if err != nil {
+			klog.Fatal("unable to build rest config based on provided path to kubeconfig file")
+		}
+	} else {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			klog.Fatal("cannot find Service Account in pod to build in-cluster rest config")
+		}
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Fatalf("Failed to instantiate Kubernetes client: %v", err)
+	}
+	return kubeClient
 }
