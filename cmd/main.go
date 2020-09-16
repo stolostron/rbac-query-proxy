b436951d0092308068dbf3803b8ed7afb8054c21
Song Song Li <ssli@redhat.com>
2020-09-16 16:44:35 +0800

    add resource authorization

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"net/http"
 	"net/http/httputil"
@@ -12,7 +13,7 @@ import (
 	"github.com/spf13/pflag"
 	"k8s.io/klog"
 
-	"github.com/open-cluster-management/rbac-query-proxy/pkg/metric"
+	"github.com/open-cluster-management/rbac-query-proxy/pkg/util"
 )
 
 const (
@@ -49,10 +50,35 @@ func main() {
 	klog.Infof("metrics server is: %s", cfg.metricServer)
 	klog.Infof("kubeconfig is: %s", cfg.kubeconfigLocation)
 
+	// watch all managed clusters
+	go util.WatchManagedCluster()
+
 	http.HandleFunc("/", handleRequestAndRedirect)
 	if err := http.ListenAndServe(cfg.listenAddress, nil); err != nil {
 		klog.Fatalf("failed to ListenAndServe: %v", err)
 	}
+}
+
+func ErrorHandle(rw http.ResponseWriter, req *http.Request, err error) {
+	token := req.Header.Get("acm-access-token-cookie")
+	if token == "" {
+		rw.WriteHeader(http.StatusUnauthorized)
+	}
+}
+
+func ModifyResponse(r *http.Response) error {
+	token := r.Request.Header.Get("acm-access-token-cookie")
+	if token == "" {
+		return errors.New("found unauthorized user")
+	}
+
+	projectList := util.FetchUserProjectList(token)
+	if len(projectList) == 0 || len(util.GetAllManagedClusterNames()) == 0 {
+		r.Body = http.NoBody
+		return errors.New("no project found")
+	}
+
+	return nil
 }
 
 // Serve a reverse proxy for a given url
@@ -64,14 +90,15 @@ func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request
 
 	// create the reverse proxy
 	proxy := httputil.NewSingleHostReverseProxy(url)
-
+	proxy.ModifyResponse = ModifyResponse
+	proxy.ErrorHandler = ErrorHandle
 	// Update the headers to allow for SSL redirection
 	req.URL.Host = url.Host
 	req.URL.Scheme = url.Scheme
 	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
 	req.Host = url.Host
 
-	metric.ModifyMetricsQueryParams(req)
+	util.ModifyMetricsQueryParams(req)
 	proxy.ServeHTTP(res, req)
 }
 
