43b56c0406f0929ffe7fc9c8fc5c12cb1a82ffc8
Song Song Li <ssli@redhat.com>
2020-12-11 11:35:51 +0800

    enable e2e

--- cicd-scripts/run-e2e-tests.sh
+++ cicd-scripts/run-e2e-tests.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 # Copyright (c) 2020 Red Hat, Inc.
 
-echo "<repo>/<component>:<tag> : $1"
+WORKDIR=`pwd`
+cd ${WORKDIR}/..
+git clone https://github.com/open-cluster-management/observability-kind-cluster.git
+cd observability-kind-cluster
+./setup.sh
+if [ $? -ne 0 ]; then
+    echo "Cannot setup environment successfully."
+    exit 1
+fi
 
-git config --global url."https://$GITHUB_TOKEN@github.com/open-cluster-management".insteadOf  "https://github.com/open-cluster-management"
+go get -u github.com/onsi/ginkgo/ginkgo
 
+export KUBECONFIG=$HOME/.kube/kind-config-hub
+export IMPORT_KUBECONFIG=$HOME/.kube/kind-config-spoke
+export SKIP_INSTALL_STEP=true
+export SKIP_UNINSTALL_STEP=true
+
+cd ${WORKDIR}
+
+git clone https://github.com/open-cluster-management/observability-gitops.git
+
+printf "options:" >> resources/options.yaml
+printf "\n  hub:" >> resources/options.yaml
+printf "\n    baseDomain: placeholder" >> resources/options.yaml
+printf "\n    masterURL: https://127.0.0.1:32806" >> resources/options.yaml
+printf "\n    grafanaURL: http://127.0.0.1" >> resources/options.yaml
+printf "\n    grafanaHost: grafana-test" >> resources/options.yaml
+printf "\n  clusters:" >> resources/options.yaml
+printf "\n    - name: spoke" >> resources/options.yaml
+printf "\n      masterURL: https://127.0.0.1:32807" >> resources/options.yaml
+
+ginkgo --skip="should work in basic mode \(reconcile/g0\)" -v ./pkg/tests -- -options=../../resources/options.yaml -v=3
+
+cat ./pkg/tests/results.xml | grep failures=\"0\" | grep errors=\"0\"
+if [ $? -ne 0 ]; then
+    echo "Cannot pass all test cases."
+    cat results.xml
+    exit 1
+fi
